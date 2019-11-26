5e244163031f0ab7d8349da55a5301c18da858da
shirley <hsinshan.hsieh@rakuten.com>
2019-11-26 13:54:38 +0800

    react add sort method and column style

--- client/src/components/stockBoard.js
+++ client/src/components/stockBoard.js
@@ -27,9 +27,9 @@ export default class StockBoard extends React.Component {
 					<td>{stock.floor_price}</td>
 					<td>{stock.yesterday_closing_price}</td>
 					<td>{stock.today_closing_price}</td>
-					<td>{stock.volumes}</td>
-					<td>{stock.flunctuation}</td>
-					<td>{stock.flunctuation_rate}</td>
+					<td className="volumes">{stock.volumes}</td>
+					<td className={this.getFlutcuationClass(stock.fluctuation)}>{stock.fluctuation}</td>
+					<td>{stock.fluctuation_rate}</td>
 				</tr>
 			)
 		})
@@ -42,6 +42,10 @@ export default class StockBoard extends React.Component {
 			return (
 				<option value = {stock.stock_id} key = {stock.stock_id}>{stock.stock_id + ' ' + stock.name}</option>
 			)
+		})
+		const headers = ['日期', '排', '代號', '名稱', '開盤價', '最高價', '最低價', '昨收盤', '今收盤', '成交量','漲跌', '漲跌幅']
+		const tableHeader = headers.map( (header, i) => {
+			return <td className="tableHeader" key={header} onClick={() => { this.onSort(i) }}>{header}</td>
 		})
 		return(
 			<div>
@@ -58,18 +62,7 @@ export default class StockBoard extends React.Component {
 				<table>
 					<thead>
 						<tr>
-							<td>日期</td>
-							<td>排</td>
-							<td>代號</td>
-							<td>名稱</td>
-							<td>開盤價</td>
-							<td>最高價</td>
-							<td>最低價</td>
-							<td>昨收盤</td>
-							<td>今收盤</td>
-							<td>成交量</td>
-							<td>漲跌</td>
-							<td>漲跌幅</td>
+							{tableHeader}
 						</tr>
 					</thead>
 					<tbody>
@@ -139,6 +132,31 @@ export default class StockBoard extends React.Component {
 	}
 	changeSid = (e) => {
 		this.setState({selectedSid: e.target.value})
+	}
+	onSort = (i) => {
+		const data = this.state.stocksData
+		const keys = ['created_at','number', 'stock_id', 'name', 'opening_price', 'highest_price', 'floor_price', 'yesterday_closing_price', 'today_closing_price', 'volumes', 'fluctuation', 'fluctuation_rate']
+		data.sort((a,b) => {
+			switch(i) {
+				case 0:
+					return new Date(a[keys[i]]) >= new Date(b[keys[i]]) 
+				case 3:
+					return a[keys[i]].localeCompare(b[keys[i]]) 
+				case 10:
+					return Number(a[keys[i]].split(' ')[1]) >=  Number(b[keys[i]].split(' ')[1]) ? 1 : -1
+				case 11:
+					return Number(a[keys[i]].split('%')[0]) <=  Number(b[keys[i]].split('%')[0]) ? 1 : -1
+				default:
+					return Number(a[keys[i]]) <=  Number(b[keys[i]]) ? 1 : -1
+			}
+			
+		}) 
+		this.setState({stocksData: data})
+	}
+	getFlutcuationClass = (data) => {
+		const prefix = data[0]
+		if (prefix === '▲') return 'positive'
+		if (prefix === '▼') return 'negative'
 	}
 }
 
